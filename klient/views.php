3e92ac0379565a841becf393fdec14b9155f8fbb
TheMrDraven <dominixx234@gmail.com>
2017-01-26 16:04:54 +0100

    Klient - FrontEnd

---
+++ klient/views.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+
+
+        <div class="menu">
+          <form action="selectTable.php" method="get">
+          <button class="menu-btn" name="tabela" type="submit" value="0">
+            Lista
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="1">
+            Przywileje i kary
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="2">
+            Wyroki
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="3">
+            Oddziały
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="4">
+            Cele
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="5">
+            Obowiązki
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="6">
+            Zaopatrzenie
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="7">
+            Pracownicy
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="8">
+            Grupa
+          </button>
+          <button class="menu-btn" name="tabela" type="submit" value="9">
+            Więźniowie
+          </button>
+          </form>
+        </div>
+        <div class="mode">
+          <a href="index.php"><div class="btn-mode">
+            Print table
+          </div></a>
+          <a href="views.php"><div class="btn-mode">
+            Views
+          </div></a>
+          <a href="selects.php"><div class="btn-mode">
+            Selects
+          </div></a>
+          <a href="inserts.php"><div class="btn-mode">
+            Inserts
+          </div></a>
+          <a href="drop.php"><div class="btn-mode">
+            Drop Table
+          </div></a>
+          <a href="delete.php"><div class="btn-mode">
+            Delete
+          </div></a>
+        </div>
+
+
+        <div class="content">
+          <form class="widok" action="selectTable.php" method="get">
+            Widoki:
+            <button class="content-btn" name="tabela" type="submit" value="10">
+              Widok dyrektora
+            </button>
+            <button class="content-btn" name="tabela" type="submit" value="11">
+              Widok straznika
+            </button>
+            <button class="content-btn" name="tabela" type="submit" value="12">
+              Widok zaopatrzeniowca
+            </button>
+            <button class="content-btn" name="tabela" type="submit" value="13">
+              Widok pracownika kadr
+            </button>
+            <button class="content-btn" name="tabela" type="submit" value="14">
+              Widok recydywistów
+            </button>
+            <button class="content-btn" name="tabela" type="submit" value="15">
+              Widok dostępnych cel
+            </button>
+            <br><br>
+            Napisz wlasny widok:
+            <input class="customInput" type="text" name="createView" value="CREATE VIEW nazwa AS ..."><br>
+            <button class="content-btn" type="subnit">Stworz i wyswietl</button>
+          </form>
+
+        </div>
+    </body>
+</html>
